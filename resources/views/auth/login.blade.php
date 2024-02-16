bf4d159b0c7253efac5770ae9a4a85141b6a8638
pinkiwinkie <elrumor99@gmail.com>
2024-02-16 16:30:35 +0100

    login styles

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,25 +1,25 @@
 @extends('plantilla')
 @section('titulo', 'Login')
 @section('contenido')
-<h1>Login</h1>
+<h1 class="col-12 d-flex justify-content-center align-items-center mt-2">Login</h1>
 @if (!empty($error))
-<div class="text-danger">
+<div class="text-danger ">
 {{ $error }}
 </div>
 @endif
-<form action="{{ route('login') }}" method="POST">
+<form class="col-12 d-flex flex-column justify-content-center align-items-center" action="{{ route('login') }}" method="POST">
 @csrf
-<div class="form-group">
+<div class="form-group col-6">
 <label for="email">Email:</label>
 <input type="text" class="form-control"
 name="email" id="email" />
 </div>
-<div class="form-group">
+<div class="form-group col-6">
 <label for="password">Password:</label>
 <input type="password" class="form-control"
 name="password" id="password" />
 </div>
 <input type="submit" name="enviar" value="Enviar"
-class="btn btn-dark btn-block">
+class="btn btn-dark btn-block col-6 mt-3">
 </form>
 @endsection
